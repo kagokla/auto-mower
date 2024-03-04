739147b7f4469fc72389617f73b950ce42fe862c
kagokla <kagokla@extremenetworks.com>
2024-03-04 19:46:05 +0100

    Fix and enhance the Controller's test

--- src/test/java/com/github/kagokla/automower/controller/MowerControllerTest.java
+++ src/test/java/com/github/kagokla/automower/controller/MowerControllerTest.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.github.kagokla.automower.model.dto.CommandRequestDTO;
 import com.github.kagokla.automower.model.dto.CommandResponseDTO;
 import com.github.kagokla.automower.service.MowingService;
+import com.github.kagokla.automower.spring.JacksonConfig;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -12,16 +13,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 import java.util.List;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(MowerController.class)
+@WebMvcTest
+@ContextConfiguration(classes = {MowerController.class, JacksonConfig.class})
 class MowerControllerTest {
 
     private static final ObjectMapper objectMapper = new ObjectMapper();
@@ -44,7 +49,8 @@ class MowerControllerTest {
         final var initialPosition = "12N";
         final var instructions = "LFLFLFLFF";
         final var commandRequest = buildCommandRequestDTO(area, initialPosition, instructions);
-        final var commandResponse = buildCommandResponseDTO(area, initialPosition, instructions, "13N");
+        final var finalPosition = "13N";
+        final var commandResponse = buildCommandResponseDTO(area, initialPosition, instructions, finalPosition);
 
         Mockito.when(mowingService.processCommand(any())).thenReturn(commandResponse);
 
@@ -54,7 +60,13 @@ class MowerControllerTest {
                         .content(objectMapper.writeValueAsString(commandRequest))
                         .contentType(MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$").isNotEmpty());
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(jsonPath("$").isNotEmpty())
+                .andExpect(jsonPath("$.area").value(area))
+                .andExpect(jsonPath("$.mowers", hasSize(1)))
+                .andExpect(jsonPath("$.mowers[0].initial_position").value(initialPosition))
+                .andExpect(jsonPath("$.mowers[0].instructions").value(instructions))
+                .andExpect(jsonPath("$.mowers[0].final_position").value(finalPosition));
     }
 
     @Test
@@ -73,7 +85,8 @@ class MowerControllerTest {
                 .perform(post("/auto-mowers")
                         .content(objectMapper.writeValueAsString(commandRequest))
                         .contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isBadRequest());
+                .andExpect(status().isBadRequest())
+                .andDo(MockMvcResultHandlers.print());
     }
 
     private CommandRequestDTO buildCommandRequestDTO(
